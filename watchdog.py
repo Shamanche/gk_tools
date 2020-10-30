205c604eb90e3d59bdd434f9954e4562cc4d37ca
Shamanche <72706516+Shamanche@users.noreply.github.com>
2020-10-30 17:32:52 +0300

    добавил обработку исключений

--- watchdog.py
+++ watchdog.py
@@ -1,14 +1,18 @@
 import requests
 import time
-wait = 5
-url = 'https://loyalty.gorkarta.ru/RS.Loyalty.Service/RSLoyaltyService.svc?wsdl'
+wait = 2
+url = 'https://test.gorkarta.ru/RS.Loyalty.Service/RSLoyaltyService.svc?wsdl'
 
 correct_answer = '<wsdl:message name="IRSLoyaltyService_Ping_InputMessage">'
-
-for i in range(5):
-    r = requests.get(url, timeout=5)
-##    print (r.status_code)
-    is_online = correct_answer in r.text
-    print(f'Время {time.ctime()}. Ответ сервера: {is_online}')
-    print(f'Задержка {wait} сек..')
-    time.sleep(wait)
+print('Старт')
+for i in range(30):
+    try:
+        r = requests.get(url, timeout=5)
+    except requests.exceptions.ConnectTimeout as e:
+        print ('Исключение: ', e)
+        print( '+++ TimeoutError +++')
+    else:
+        is_online = correct_answer in r.text
+        print(f'Время {time.ctime()}. Ответ сервера: {is_online}')
+        print(f'Задержка {wait} сек..')
+        time.sleep(wait)
