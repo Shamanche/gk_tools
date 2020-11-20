5b059fa34c0aa1e5e55c79edbd054545b3abb67d
Shamanche <72706516+Shamanche@users.noreply.github.com>
2020-11-20 19:10:53 +0300

    test phone_list

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-from sys import argv
+import csv
 
-f, filename = argv
+filename = '2020-11-20_11-32-27.csv'
+##filename = 'test_2020-11-20_11-32-27.csv'
+text = 'CityCard: код подтверждения карты:'
 
-print (filename)
+##with open (filename, 'r' ) as f:
+##    reader = csv.reader(f)
+##    for row in reader:
+##        print (row)
+
+phone_list = []
+with open (filename, 'r') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    pass
+    for row in reader:
+        if row['Идентификатор SMS'] == 'Всего SMS':
+            break
+        else:
+            if text in row['Текст SMS']:
+                phone_list.append(row['Номер телефона'])
 
